9f61d6820a2706bb168ca5b367a24aa9d1dbdcd3
wisecommunity <wisecaseteam@gmail.com>
2025-12-18 22:49:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: "/WISKEY-NDHU-2025/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
